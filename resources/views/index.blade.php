0ee8b157beb8a863594213a51a4910bbd46bd6ac
vlandthemoir <Elthon13_v@outlook.com>
2023-05-16 11:40:52 -0600

    proyecto en blanco

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="">
+    <head>
+        <meta charset="utf-8">
+        <title>Laravel</title>
+
+    </head>
+    <body>
+        <div>
+            <h1>hola mundo</h1>
+        </div>
+    </body>
+</html>
